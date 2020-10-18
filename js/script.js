0ed74844ae8bfe79657601c18c333f786eb17f5c
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-10-18 18:22:21 +0200

    primera funcion login/logout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+const loginButton = document.querySelector('#loginButton')
+const logoutButton = document.querySelector('#logoutButton')
+
+loginButton.addEventListener('click', login)
+logoutButton.addEventListener('click', logout)
+
+
+
+
+function login() {
+    document.querySelector('#login').style.display = 'none'
+    document.querySelector('#todoList').style.display = 'block'
+}
+
+function logout() {
+    document.querySelector('#login').style.display = 'flex'
+    document.querySelector('#todoList').style.display = 'none'
+}
+
 const prueba = [{
     idTarea: 0,
     titulo: 'Estudiar JS',
@@ -5,9 +24,5 @@ const prueba = [{
 }]
 
 function printList(pLista) {
-
-
-
-
 
 }
