3a14c109d925d566822efa2ab8ba0d0a4acc7c5f
Alice López Gómez <alicelopezgomez@macbook-air-de-antonio.home>
2020-10-21 08:39:30 +0200

    anexo todo.js

---
+++ js/todo.js
@@ -0,0 +1,15 @@
+const todo = new Array(
+    {
+        tarea: "Estudiar JS",
+        prioridad: "Urgente"
+    },
+    {
+        tarea: "Hacer máster online",
+        prioridad: "Diario"
+    },
+    {
+        tarea: "Cuidar el Jardin",
+        prioridad: "Mensual"
+    },
+
+)
